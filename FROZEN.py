e48093300fed1020259706f26b4d518d4423b953
Elias Wang <ewang921@gmail.com>
2021-09-10 16:05:11 -0700

    move some methods into base Objective class, some import cleanup

--- FROZEN.py
+++ FROZEN.py
@@ -1,21 +1,13 @@
-import argparse
-import datetime
-import os
-import pickle
 import numpy as np
-import logging
-from hyperopt import STATUS_OK
 
-from physopt.utils import PhysOptObjective
-import physion.modules.frozen as modules
-from physopt.models.physion.config import get_frozen_physion_cfg
-from physopt.models.pydata import TDWDataset as TDWDatasetBase
-from physopt.models.config import get_data_cfg 
-from torch.utils.data import DataLoader
 import torch
-import mlflow
+from torch.utils.data import DataLoader
 
-NUM_EPOCHS = 2 # TODO: remove hard-coded num epochs
+from physopt.utils import PytorchPhysOptObjective
+from physopt.models.pydata import TDWDataset as TDWDatasetBase
+
+import physion.modules.frozen as modules
+from physion.config import get_frozen_physion_cfg
 
 class TDWDataset(TDWDatasetBase): # TODO: move to physion.utils
     def __getitem__(self, index):
@@ -29,7 +21,7 @@ class TDWDataset(TDWDatasetBase): # TODO: move to physion.utils
             })
         return sample
 
-class Objective(PhysOptObjective):
+class Objective(PytorchPhysOptObjective):
     def __init__(self,
             exp_key,
             seed,
@@ -44,58 +36,11 @@ class Objective(PhysOptObjective):
             ):
         super().__init__(exp_key, seed, train_data, feat_data, output_dir, extract_feat, debug)
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.experiment_name = '{}Frozen{}'.format(encoder, dynamics) # mlflow experiment name tied to the Objective class TODO: or maybe just have everything in one experiment, and only use run_name
         self.init_seed()
 
-        self.model = self.get_model(encoder, dynamics)
+        model_cfg = {'encoder': encoder, 'dynamics': dynamics} # TODO
+        self.model = self.get_model(model_cfg)
         self.model = self.load_model()
-
-    def dynamics(self):
-        mlflow.set_experiment(self.experiment_name)
-        mlflow.start_run(run_name=self.exp_key)
-
-        trainloader = self.get_dataloader(self.dynamics_data['train'], train=True)
-        best_loss = 1e9
-        for epoch in range(NUM_EPOCHS): 
-            logging.info('Starting epoch {}/{}'.format(epoch+1, NUM_EPOCHS))
-            running_loss = 0.
-            for i, data in enumerate(trainloader):
-                loss = self.train_step(data)
-                running_loss += loss
-                avg_loss = running_loss/(i+1)
-                print(avg_loss)
-            mlflow.log_metric(key='avg_loss', value=avg_loss, step=epoch)
-            # TODO: add validation
-
-        if avg_loss < best_loss:
-            best_loss = avg_loss
-            logging.info('Saving model with loss {} at epoch {}'.format(best_loss, epoch))
-            self.save_model()
-        mlflow.end_run()
-
-    def readout(self):
-        mlflow.set_experiment(self.experiment_name)
-        mlflow.start_run(run_name=self.exp_key)
-
-        assert os.path.isfile(self.model_file), 'No model ckpt found, cannot extract features'
-        self.model.eval() # set to eval mode
-
-        trainloader = self.get_dataloader(self.readout_data['train'], train=False)
-        self.extract_feats(trainloader, self.train_feature_file)
-        testloader = self.get_dataloader(self.readout_data['test'], train=False)
-        self.extract_feats(testloader, self.test_feature_file)
-
-        self.compute_metrics()
-
-        mlflow.end_run()
-    
-    def extract_feats(self, dataloader, feature_file):
-        extracted_feats = []
-        for i, data in enumerate(dataloader):
-            output = self.test_step(data)
-            extracted_feats.append(output)
-        pickle.dump(extracted_feats, open(feature_file, 'wb')) 
-        print('Saved features to {}'.format(feature_file))
 
     def get_dataloader(self, datapaths, train=True):
         cfg = get_frozen_physion_cfg(debug=self.debug)
@@ -111,25 +56,13 @@ class Objective(PhysOptObjective):
         dataloader = DataLoader(dataset, batch_size=cfg.BATCH_SIZE, shuffle=train)
         return dataloader
 
-    def get_model(self, encoder, dynamics):
-        model =  modules.FrozenPhysion(encoder, dynamics).to(self.device)
+    def get_model(self, model_cfg):
+        model =  modules.FrozenPhysion(model_cfg['encoder'], model_cfg['dynamics']).to(self.device)
         # model = torch.nn.DataParallel(model) # TODO: multi-gpu doesn't work yet, also for loading
         return model
 
-    def load_model(self):
-        if os.path.isfile(self.model_file): # load existing model ckpt TODO: add option to disable reloading
-            self.model.load_state_dict(torch.load(self.model_file))
-            logging.info('Loaded existing ckpt')
-        else:
-            torch.save(model.state_dict(), self.model_file) # save initial model
-            logging.info('Training from scratch')
-        return self.model
-
-    def save_model(self):
-        torch.save(self.model.state_dict(), self.model_file)
-        logging.info('Saved model checkpoint to: {}'.format(self.model_file))
-
     def train_step(self, data):
+        self.model.train() # set to train mode
         inputs = data['input_images'].to(self.device)
         labels = self.model.get_encoder_feats(data['label_image'].to(self.device))
         criterion = torch.nn.MSELoss()
@@ -144,6 +77,7 @@ class Objective(PhysOptObjective):
         return loss.item()
 
     def test_step(self, data):
+        self.model.eval() # set to eval mode
         with torch.no_grad():
             images = data['images'].to(self.device)
             state_len = data['input_images'].shape[1] # TODO: hacky
@@ -168,11 +102,6 @@ class Objective(PhysOptObjective):
             'stimulus_name': stimulus_name,
             }
         return output
-
-    def init_seed(self):
-        np.random.seed(self.seed)
-        torch.manual_seed(self.seed)
-        torch.cuda.manual_seed(self.seed)
 
 class VGGFrozenIDObjective(Objective):
     def __init__(self, *args, **kwargs):
