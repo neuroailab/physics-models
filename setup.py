ea73034573fdf97b3379974e963c8c67575cee97
Eli Wang <ewang921@gmail.com>
2021-09-19 22:34:12 -0700

    update package requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from distutils.core import setup
 setup(
     name='physion',
     version='1.0',
-    py_modules=['physion'],
+    packages=['physion'],
+    install_requires=[
+        'torch',
+        'torchvision',
+        'h5py',
+        'Pillow',
+        'clip @ git+https://github.com/openai/CLIP.git@main',
+    ]
 )
