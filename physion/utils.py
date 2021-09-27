957c1ec719de959affd058c4d8f0112034271b97
Eli Wang <ewang921@gmail.com>
2021-09-27 15:01:00 -0700

    simplify loading/saving - now takes model_file as input

--- physion/utils.py
+++ physion/utils.py
@@ -12,22 +12,15 @@ class PytorchPhysOptObjective(PhysOptObjective):
         super().__init__(*args, **kwargs)
         self.init_seed()
 
-    def load_model(self):
-        if os.path.isfile(self.model_file): # load existing model ckpt TODO: add option to disable reloading
-            self.model.load_state_dict(torch.load(self.model_file))
-            logging.info('Loaded existing ckpt')
-        else:
-            torch.save(self.model.state_dict(), self.model_file) # save initial model
-            logging.info('No model found, saved initial model')
+    def load_model(self, model_file):
+        assert os.path.isfile(model_file), f'Cannot find model file: {model_file}'
+        self.model.load_state_dict(torch.load(model_file))
+        logging.info(f'Loaded existing ckpt from {model_file}')
         return self.model
 
-    def save_model(self, step=None):
-        torch.save(self.model.state_dict(), self.model_file)
-        logging.info('Saved model checkpoint to: {}'.format(self.model_file))
-        if step is not None:
-            step_model_file = '_{}.'.format(step).join(self.model_file.split('.')) # create model file with step 
-            torch.save(self.model.state_dict(), step_model_file)
-            mlflow.log_artifact(step_model_file, artifact_path='model_ckpts')
+    def save_model(self, model_file):
+        logging.info(f'Saved model checkpoint to: {model_file}')
+        torch.save(self.model.state_dict(), model_file)
 
     def init_seed(self):
         np.random.seed(self.seed)
