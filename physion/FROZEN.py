8a0c66ee1f26b613930899673894a57149c9151a
Eli Wang <ewang921@gmail.com>
2021-09-27 21:24:38 -0700

    add mrr/h@1 validation

--- physion/FROZEN.py
+++ physion/FROZEN.py
@@ -1,4 +1,6 @@
 import numpy as np
+import logging
+from collections import defaultdict
 import torch
 from torch.utils.data import DataLoader
 
@@ -56,6 +58,90 @@ class Objective(PytorchPhysOptObjective):
         optimizer.step()
 
         return loss.item()
+
+    def validation(self):
+        valloader = self.get_dataloader(self.pretraining_space['test'], phase='pretraining', train=False, shuffle=False)
+        val_results = []
+        pred_states = []
+        next_states = []
+        with torch.no_grad():
+            for i, data in enumerate(valloader):
+                logging.info('Val Step: {0:>5}'.format(i+1))
+                val_res = self.val_step(data)
+                assert isinstance(val_res, dict)
+                val_results.append(val_res)
+
+                state_len = data['input_images'].shape[1]
+                states = self.model.get_encoder_feats(data['images'][:,:state_len+1].to(self.device))
+                input_state = states[:-1]
+                next_state = states[-1]
+                pred_state  = self.model.dynamics(input_state) # dynamics model predicts next latent from past latents
+
+                pred_states.append(pred_state.cpu())
+                next_states.append(next_state.cpu())
+
+            pred_state_cat = torch.cat(pred_states, dim=0)
+            next_state_cat = torch.cat(next_states, dim=0)
+
+        # convert list of dicts into single dict by aggregating with mean over values for a given key
+        val_results = {k: np.mean([res[k] for res in val_results]) for k in val_results[0]} # assumes all keys are the same across list
+        val_results.update(self.latent_eval(pred_state_cat, next_state_cat))
+        return val_results
+
+    def latent_eval(self, pred_state_cat, next_state_cat): # TODO
+        topk = [1]
+        hits_at = defaultdict(int)
+        full_size = pred_state_cat.size(0)
+
+        # Flatten object/feature dimensions
+        next_state_flat = next_state_cat.view(full_size, -1)
+        pred_state_flat = pred_state_cat.view(full_size, -1)
+
+        dist_matrix = pairwise_distance_matrix(
+            next_state_flat, pred_state_flat)
+        dist_matrix_diag = torch.diag(dist_matrix).unsqueeze(-1)
+        dist_matrix_augmented = torch.cat(
+            [dist_matrix_diag, dist_matrix], dim=1)
+
+        # Workaround to get a stable sort in numpy.
+        dist_np = dist_matrix_augmented.numpy()
+        indices = []
+        for row in dist_np:
+            keys = (np.arange(len(row)), row)
+            indices.append(np.lexsort(keys))
+        indices = np.stack(indices, axis=0)
+        indices = torch.from_numpy(indices).long()
+
+        labels = torch.zeros(
+            indices.size(0), device=indices.device,
+            dtype=torch.int64).unsqueeze(-1)
+
+        num_samples = full_size
+        print('Size of current topk evaluation batch: {}'.format(
+            full_size))
+
+        for k in topk:
+            match = indices[:, :k] == labels
+            num_matches = match.sum()
+            hits_at[k] += num_matches.item()
+
+        match = indices == labels
+        _, ranks = match.max(1)
+
+        reciprocal_ranks = torch.reciprocal(ranks.double() + 1)
+        rr_sum = reciprocal_ranks.sum().item()
+
+        for k in topk:
+            print('Hits @ {}: {}'.format(k, hits_at[k] / float(num_samples)))
+
+        print('MRR: {}'.format(rr_sum / float(num_samples)))
+
+        val_results = {
+            'Hits_at_1': hits_at[1] / float(num_samples),
+            'MRR': rr_sum / float(num_samples),
+            'num_samples': num_samples,
+            }
+        return val_results
 
     def val_step(self, data): # TODO: reduce duplication with train_step
         self.model.eval() # set to eval mode
@@ -162,3 +248,12 @@ class pDINO_LSTMObjective(Objective):
     model_name = 'pDINO_LSTM'
     def get_model(self):
         return get_frozen_model('dino', 'lstm').to(self.device)
+
+def pairwise_distance_matrix(x, y): # TODO
+    num_samples = x.size(0)
+    dim = x.size(1)
+
+    x = x.unsqueeze(1).expand(num_samples, num_samples, dim)
+    y = y.unsqueeze(0).expand(num_samples, num_samples, dim)
+
+    return torch.pow(x - y, 2).sum(2)
