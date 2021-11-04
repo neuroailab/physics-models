3a454559e2f2984016d3414d3e241c70eb3deb48
eli <eliaszwang@outlook.com>
2021-11-04 03:35:25 +0000

    allow settings individually for each phase/mode, add suffix for scenarios

--- physion/data_space/data_space.py
+++ physion/data_space/data_space.py
@@ -2,147 +2,153 @@ import os
 import socket
 import yaml
 
-def build_paths(name, scenarios, filepattern, traindir, testdir):
-    dirname =  os.path.dirname(__file__)
-    hostname = socket.gethostname()
-    basedir_file = os.path.join(dirname, 'basedir.yaml')
-    if os.path.isfile(basedir_file):
-        basedir_dict = yaml.safe_load(open(basedir_file, 'rb'))
-        basedir = basedir_dict.get(hostname)
-    if basedir is not None:
-        if not os.path.isabs(traindir):
-            traindir = os.path.join(basedir, traindir)
-        if not os.path.isabs(testdir):
-            testdir = os.path.join(basedir, testdir)
-    return {
-        'name': name, 
-        'train': [os.path.join(traindir, scenario, filepattern) for scenario in scenarios], 
-        'test': [os.path.join(testdir, scenario, filepattern) for scenario in scenarios],
-        } 
+class DataManager():
+    def __init__(self, data_settings):
+        self.data_settings = data_settings
+
+    def get_setting(self, setting, mode, phase):
+        try:
+            val = self.data_settings[phase][mode][setting]
+        except KeyError:
+            try:
+                val = self.data_settings[phase][setting]
+            except KeyError:
+                try:
+                    val = self.data_settings[setting]
+                except KeyError:
+                    print(self.data_settings)
+                    print(f'{setting} not found for phase {phase} and mode {mode}')
+                    raise
+        return val
+
+    def get_scenarios(self, mode, phase):
+        scenarios = self.get_setting('scenarios', mode, phase)
+        suffix = self.get_setting('suffix', mode, phase)
+        assert isinstance(scenarios, list), f'Scenarios for {phase} {mode} has type {type(scenarios)}, expected list'
+        return [scenario + suffix for scenario in scenarios]
+
+    def get_data_dir(self, mode, phase):
+        data_dir = self.get_setting('dir', mode, phase)
+        if not os.path.isabs(data_dir):
+            data_dir = self.add_basedir(data_dir)
+        return data_dir
+
+    def get_file_pattern(self, mode, phase):
+        return self.get_setting('file_pattern', mode, phase)
+
+    @staticmethod
+    def add_basedir(reldir):
+        dirname =  os.path.dirname(__file__)
+        hostname = socket.gethostname()
+        basedir_file = os.path.join(dirname, 'basedir.yaml')
+        if os.path.isfile(basedir_file):
+            basedir_dict = yaml.safe_load(open(basedir_file, 'rb'))
+            assert hostname in basedir_dict, f'{hostname} not found in {basedir_file}'
+            basedir = basedir_dict.get(hostname)
+        return os.path.join(basedir, reldir)
+
+    def build_paths(self, name, scenarios, phase):
+        res = {
+            'name': name, 
+            } 
+        for mode in ['train', 'test']:
+            curr_scenarios = scenarios[mode]
+            if not isinstance(curr_scenarios, list):
+                assert isinstance(curr_scenarios, str)
+                curr_scenarios = [curr_scenarios]
+            data_dir = self.get_data_dir(mode, phase)
+            file_pattern = self.get_file_pattern(mode, phase)
+            res[mode] = [os.path.join(data_dir, scenario, file_pattern) for scenario in curr_scenarios]
+        return res
+
+def get_readout_paths(data_manager, readout_protocol, pretraining_train_scenario):
+    readout_train_scenarios = data_manager.get_scenarios('train', 'readout')
+    readout_test_scenarios = data_manager.get_scenarios('test', 'readout')
+    if readout_protocol == 'full':
+        readout_paths = [data_manager.build_paths(readout_train_scenario, {'train': readout_train_scenario, 'test': readout_test_scenario}, 'readout') for readout_train_scenario, readout_test_scenario in zip(readout_train_scenarios, readout_test_scenarios)]
+    else: # "minimal" protocal matches readout train scenario to pretraining train scenario
+        pretrain_train_wo_suffix = pretraining_train_scenario.replace(data_manager.get_setting('suffix', 'train', 'pretraining'), '', 1)
+        assert pretrain_train_wo_suffix in readout_train_scenarios, '{} not in {}, but using "{}" readout protocol'.format(pretrain_train_wo_suffix, readout_train_scenarios, readout_protocol)
+        readout_train_scenario = pretrain_train_wo_suffix
+        readout_test_scenario = readout_test_scenarios[readout_train_scenarios.index(pretrain_train_wo_suffix)]
+        readout_paths = [data_manager.build_paths(readout_train_scenario, {'train': readout_train_scenario, 'test': readout_test_scenario}, 'readout')]
+    return readout_paths
 
 def get_only_space(
-    pretraining_scenarios,
-    readout_scenarios,
+    data_manager,
     readout_protocol,
-    pretraining_train_dir,
-    pretraining_test_dir,
-    pretraining_file_pattern,
-    readout_train_dir,
-    readout_test_dir,
-    readout_file_pattern,
     ):
     data_spaces = []
-    for scenario in pretraining_scenarios:
-        if readout_protocol == 'full':
-            curr_readout_scenarios = readout_scenarios
-        else:
-            assert scenario in readout_scenarios, '{} not in {}, but using "{}" readout protocol'.format(scenario, readout_scenarios, readout_protocol)
-            curr_readout_scenarios = [scenario]
+    pretraining_train_scenarios = data_manager.get_scenarios('train', 'pretraining')
+    pretraining_test_scenarios = data_manager.get_scenarios('test', 'pretraining')
+    for pretraining_train_scenario, pretraining_test_scenario in zip(pretraining_train_scenarios, pretraining_test_scenarios):
         space = {
-            'pretraining': build_paths(scenario, [scenario], pretraining_file_pattern, pretraining_train_dir, pretraining_test_dir),
-            'readout': [build_paths(scenario, [scenario], readout_file_pattern, readout_train_dir, readout_test_dir) for scenario in curr_readout_scenarios],
+            'pretraining': data_manager.build_paths(pretraining_train_scenario, {'train': pretraining_train_scenario, 'test': pretraining_test_scenario}, 'pretraining'),
+            'readout': get_readout_paths(data_manager, readout_protocol, pretraining_train_scenario),
             }
         data_spaces.append(space)
     return data_spaces
 
 def get_abo_space(
-    pretraining_scenarios,
-    readout_scenarios,
+    data_manager,
     readout_protocol,
-    pretraining_train_dir,
-    pretraining_test_dir,
-    pretraining_file_pattern,
-    readout_train_dir,
-    readout_test_dir,
-    readout_file_pattern,
     ):
     data_spaces = []
-    for scenario in pretraining_scenarios:
-        assert len(pretraining_scenarios) > 1, 'Must have more than one scenario to do all-but-one protocol.'
-        abo_scenarios = [s for s in pretraining_scenarios if s is not scenario]
-        if readout_protocol == 'full':
-            curr_readout_scenarios = readout_scenarios
-        else:
-            assert scenario in readout_scenarios, '{} not in {}, but using "{}" readout protocol'.format(scenario, readout_scenarios, readout_protocol)
-            curr_readout_scenarios = [scenario]
+    pretraining_train_scenarios = data_manager.get_scenarios('train', 'pretraining')
+    pretraining_test_scenarios = data_manager.get_scenarios('test', 'pretraining')
+    readout_train_scenarios = data_manager.get_scenarios('train', 'readout')
+    readout_test_scenarios = data_manager.get_scenarios('test', 'readout')
+    assert len(pretraining_train_scenarios) > 1, 'Must have more than one scenario to do all-but-one pretraining protocol.' # just check train since train and test should be same length
+    for pretraining_train_scenario, pretraining_test_scenario in zip(pretraining_train_scenarios, pretraining_test_scenarios):
+        # build abo scenarios
+        abo_pretraining_scenarios = list(zip(pretraining_train_scenarios, pretraining_test_scenarios))
+        abo_pretraining_scenarios.remove((pretraining_train_scenario, pretraining_test_scenario))
+        abo_pretraining_train_scenarios, abo_pretraining_test_scenarios = [list(t) for t in zip(*abo_pretraining_scenarios)]
+
         space = {
-            'pretraining': build_paths('no_'+scenario, abo_scenarios, pretraining_file_pattern, pretraining_train_dir, pretraining_test_dir), # train on all but the scenario
-            'readout': [build_paths(scenario, [scenario], readout_file_pattern, readout_train_dir, readout_test_dir) for scenario in curr_readout_scenarios],
+            'pretraining': data_manager.build_paths('no_'+pretraining_train_scenario, {'train': abo_pretraining_train_scenarios, 'test': abo_pretraining_test_scenarios}, 'pretraining'),
+            'readout': get_readout_paths(data_manager, readout_protocol, pretraining_train_scenario),
             }
         data_spaces.append(space)
     return data_spaces
 
 def get_all_space(
-    pretraining_scenarios,
-    readout_scenarios,
-    readout_protocol,
-    pretraining_train_dir,
-    pretraining_test_dir,
-    pretraining_file_pattern,
-    readout_train_dir,
-    readout_test_dir,
-    readout_file_pattern,
+    data_manager,
     ):
-    assert len(pretraining_scenarios) > 1, 'Must have more than one scenario to do all protocol.'
+    pretraining_train_scenarios = data_manager.get_scenarios('train', 'pretraining')
+    pretraining_test_scenarios = data_manager.get_scenarios('test', 'pretraining')
+    readout_train_scenarios = data_manager.get_scenarios('train', 'readout')
+    readout_test_scenarios = data_manager.get_scenarios('test', 'readout')
+    assert len(pretraining_train_scenarios) > 1, f'Must have more than one scenario to do all pretraining protocol.' # just check train since train and test should be same length
+    pretraining_train_suffix = data_manager.get_setting('suffix', 'train', 'pretraining')
     space = {
-        'pretraining': build_paths('all', pretraining_scenarios, pretraining_file_pattern, pretraining_train_dir, pretraining_test_dir), # train on all scenarios
-        'readout': [build_paths(scenario, [scenario], readout_file_pattern, readout_train_dir, readout_test_dir) for scenario in readout_scenarios], # readout on each scenario individually
+        'pretraining': data_manager.build_paths('all'+pretraining_train_suffix, {'train': pretraining_train_scenarios, 'test': pretraining_test_scenarios}, 'pretraining'),
+        'readout': [data_manager.build_paths(readout_train_scenario, {'train': readout_train_scenario, 'test': readout_test_scenario}, 'readout') for readout_train_scenario, readout_test_scenario in zip(readout_train_scenarios, readout_test_scenarios)]
         }
     data_spaces = [space]
     return data_spaces
 
 def get_data_spaces(
-    pretraining_train_dir,
-    pretraining_test_dir,
-    pretraining_scenarios,
-    readout_train_dir,
-    readout_test_dir,
-    pretraining_file_pattern='*.hdf5',
     pretraining_protocols=('all', 'abo', 'only'),
-    readout_scenarios=None,
-    readout_file_pattern=None,
     readout_protocol='minimal', # {'full'|'minimal'}: 'minimal' only does readout on matching scenario to pretraining
+    **data_settings
     ):
-    if readout_file_pattern is None:
-        readout_file_pattern = pretraining_file_pattern
-    if readout_scenarios is None:
-        readout_scenarios = pretraining_scenarios
+    data_manager = DataManager(data_settings)
 
     data_spaces = [] # only pretraining and readout spaces, without seed
     if 'only' in pretraining_protocols:
         data_spaces.extend(get_only_space(
-            pretraining_scenarios,
-            readout_scenarios,
+            data_manager,
             readout_protocol,
-            pretraining_train_dir,
-            pretraining_test_dir,
-            pretraining_file_pattern,
-            readout_train_dir,
-            readout_test_dir,
-            readout_file_pattern,
             ))
     if 'abo' in pretraining_protocols:
         data_spaces.extend(get_abo_space(
-            pretraining_scenarios,
-            readout_scenarios,
+            data_manager,
             readout_protocol,
-            pretraining_train_dir,
-            pretraining_test_dir,
-            pretraining_file_pattern,
-            readout_train_dir,
-            readout_test_dir,
-            readout_file_pattern,
             ))
     if 'all' in pretraining_protocols:
         data_spaces.extend(get_all_space(
-            pretraining_scenarios,
-            readout_scenarios,
-            readout_protocol,
-            pretraining_train_dir,
-            pretraining_test_dir,
-            pretraining_file_pattern,
-            readout_train_dir,
-            readout_test_dir,
-            readout_file_pattern,
+            data_manager,
             ))
+    # print(*data_spaces, sep='\n')
     return data_spaces
