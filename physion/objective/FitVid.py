c43bd07471dd1bf5134fca8464386ccef34b2df6
Elias Wang <eliwang@stanford.edu>
2022-01-07 07:44:01 +0000

    add vis during pretraining train/val

--- physion/objective/FitVid.py
+++ physion/objective/FitVid.py
@@ -10,6 +10,7 @@ from physion.objective.objective import PytorchModel
 from physion.data.pydata import TDWDataset
 from physion.models.fitvid import FitVid
 
+N_VIS_PER_BATCH = 1
 BASE_FPS = 30
 
 class FitVidModel(PytorchModel):
@@ -31,6 +32,7 @@ class PretrainingObjective(FitVidModel, PretrainingObjectiveBase):
         super().setup()
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.pretraining_cfg.TRAIN.LR)
         self.optimizer.zero_grad()
+        self.count = 0
 
     def train_step(self, data):
         self.model.train()
@@ -46,6 +48,14 @@ class PretrainingObjective(FitVidModel, PretrainingObjectiveBase):
         if self.step % accumulation_steps == 0:
             self.optimizer.step()
             self.optimizer.zero_grad()
+
+        # save visualizations
+        frames = {
+            'gt': data['images'],
+            'sim': model_output['preds'].cpu().detach(),
+            'stimulus_name': data['stimulus_name'],
+            }
+        self.count += save_vis(frames, self.pretraining_cfg, self.output_dir, self.count, 'videos/train')
         return loss.item()
 
     def val_step(self, data):
@@ -54,33 +64,17 @@ class PretrainingObjective(FitVidModel, PretrainingObjectiveBase):
         with torch.no_grad():
             model_output = self.model(data['images'].to(self.device))
             loss = model_output['loss']
+
+        # save visualizations
+        frames = {
+            'gt': data['images'],
+            'sim': model_output['preds'].cpu(),
+            'stimulus_name': data['stimulus_name'],
+            }
+        self.count += save_vis(frames, self.pretraining_cfg, self.output_dir, self.count, 'videos/val')
+
         val_res =  {'val_loss': loss.item()}
         return val_res
-
-def add_border(arr, color=[0,255,0]):
-    assert type(arr) == np.ndarray
-    assert arr.ndim == 4 
-    assert arr.shape[3] == 3, arr.shape
-
-    width = int(0.025 * max(*arr.shape[2:]))
-    pad_width = [(0,0), (width, width), (width, width)]
-    assert len(color) == 3
-    r_cons, g_cons, b_cons = color
-    r_, g_, b_ = arr[:, :, :, 0], arr[:, :, :, 1], arr[:, :, :, 2]
-    rb = np.pad(array=r_, pad_width=pad_width, mode='constant', constant_values=r_cons)
-    gb = np.pad(array=g_, pad_width=pad_width, mode='constant', constant_values=g_cons)
-    bb = np.pad(array=b_, pad_width=pad_width, mode='constant', constant_values=b_cons)
-    arr = np.stack([rb, gb, bb], axis=-1)
-    return arr
-
-def add_rollout_border(arr, rollout_len):
-    arr_inp = arr[:-rollout_len]
-    arr_inp = add_border(arr_inp, [255, 0, 0])
-    arr_pred = arr[-rollout_len:]
-    arr_pred = add_border(arr_pred)
-    arr = np.concatenate([arr_inp, arr_pred], axis=0)
-    return arr
-    
 
 class ExtractionObjective(FitVidModel, ExtractionObjectiveBase):
     def get_readout_dataloader(self, datapaths):
@@ -120,34 +114,18 @@ class ExtractionObjective(FitVidModel, ExtractionObjectiveBase):
             observed_hs = torch.stack(observed_h_preds, 1).cpu().numpy()
             observed_preds = torch.stack(observed_preds, 1)
 
-        # save N samples in batch
-        N = min(data['images'].shape[0], 1)
-        for i in range(N):
-            rollout_len = self.pretraining_cfg.DATA.SEQ_LEN - self.pretraining_cfg.DATA.STATE_LEN
-            fn = os.path.join(self.output_dir, f'gt_{self.count}_'+data['stimulus_name'][i]+'.mp4')
-            arr = (255*torch.permute(data['images'][i], (0,2,3,1)).numpy()).astype(np.uint8)
-            arr = add_rollout_border(arr, rollout_len)
-            imageio.mimwrite(fn, arr, fps=BASE_FPS//self.pretraining_cfg.DATA.SUBSAMPLE_FACTOR, macro_block_size=None)
-            mlflow.log_artifact(fn, artifact_path='videos')
-            logging.info(f'Video written to {fn}')
-
-            fn = os.path.join(self.output_dir, f'obs_{self.count}_'+data['stimulus_name'][i]+'.mp4')
-            arr = (255*torch.permute(observed_preds[i], (0,2,3,1)).cpu().numpy()).astype(np.uint8)
-            arr = add_rollout_border(arr, rollout_len)
-            imageio.mimwrite(fn, arr, fps=BASE_FPS//self.pretraining_cfg.DATA.SUBSAMPLE_FACTOR, macro_block_size=None)
-            mlflow.log_artifact(fn, artifact_path='videos')
-            logging.info(f'Video written to {fn}')
-
-            fn = os.path.join(self.output_dir, f'sim_{self.count}_'+data['stimulus_name'][i]+'.mp4')
-            arr = (255*torch.permute(preds[i], (0,2,3,1)).cpu().numpy()).astype(np.uint8)
-            arr = add_rollout_border(arr, rollout_len)
-            imageio.mimwrite(fn, arr, fps=BASE_FPS//self.pretraining_cfg.DATA.SUBSAMPLE_FACTOR, macro_block_size=None)
-            mlflow.log_artifact(fn, artifact_path='videos')
-            logging.info(f'Video written to {fn}')
-            self.count += 1
+        # save visualizations
+        frames = {
+            'gt': data['images'],
+            'obs': observed_preds.cpu(),
+            'sim': preds.cpu(),
+            'stimulus_name': data['stimulus_name'],
+            }
+        self.count += save_vis(frames, self.pretraining_cfg, self.output_dir, self.count)
 
         labels = data['binary_labels'].cpu().numpy()[:,1:] # skip first label to match length of preds -- all the same anyways
         stimulus_name = np.array(data['stimulus_name'], dtype=object)
+        rollout_len = pretraining_cfg.DATA.SEQ_LEN - pretraining_cfg.DATA.STATE_LEN
         output = {
             'input_states': h_preds[:,:-rollout_len],
             'observed_states': observed_hs[:,-rollout_len:], 
@@ -157,3 +135,42 @@ class ExtractionObjective(FitVidModel, ExtractionObjectiveBase):
             }
         return output
             
+def add_border(arr, color=[0,255,0]):
+    assert type(arr) == np.ndarray
+    assert arr.ndim == 4 
+    assert arr.shape[3] == 3, arr.shape
+
+    width = int(0.025 * max(*arr.shape[2:]))
+    pad_width = [(0,0), (width, width), (width, width)]
+    assert len(color) == 3
+    r_cons, g_cons, b_cons = color
+    r_, g_, b_ = arr[:, :, :, 0], arr[:, :, :, 1], arr[:, :, :, 2]
+    rb = np.pad(array=r_, pad_width=pad_width, mode='constant', constant_values=r_cons)
+    gb = np.pad(array=g_, pad_width=pad_width, mode='constant', constant_values=g_cons)
+    bb = np.pad(array=b_, pad_width=pad_width, mode='constant', constant_values=b_cons)
+    arr = np.stack([rb, gb, bb], axis=-1)
+    return arr
+
+def add_rollout_border(arr, rollout_len):
+    arr_inp = arr[:-rollout_len]
+    arr_inp = add_border(arr_inp, [255, 0, 0])
+    arr_pred = arr[-rollout_len:]
+    arr_pred = add_border(arr_pred)
+    arr = np.concatenate([arr_inp, arr_pred], axis=0)
+    return arr
+
+def save_vis(frames, pretraining_cfg, output_dir, count=0, artifact_path='videos'):
+    rollout_len = pretraining_cfg.DATA.SEQ_LEN - pretraining_cfg.DATA.STATE_LEN
+    fps = BASE_FPS // pretraining_cfg.DATA.SUBSAMPLE_FACTOR
+    n_vis_per_batch = min(pretraining_cfg.BATCH_SIZE, N_VIS_PER_BATCH)
+    stimulus_name = frames.pop('stimulus_name')
+    for i in range(n_vis_per_batch):
+        for k,v in frames.items():
+            fn = os.path.join(output_dir, f'{count}_'+stimulus_name[i]+f'_{k}.mp4')
+            arr = (255*torch.permute(v[i], (0,2,3,1)).numpy()).astype(np.uint8)
+            arr = add_rollout_border(arr, rollout_len)
+            imageio.mimwrite(fn, arr, fps=fps, macro_block_size=None)
+            mlflow.log_artifact(fn, artifact_path=artifact_path)
+            logging.info(f'Video written to {fn}')
+        count += 1
+    return count
